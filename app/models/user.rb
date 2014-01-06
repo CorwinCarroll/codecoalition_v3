a71234aeb422b4880866dd5f88bb67f3abd7e391
Corwin Carroll <corwin.p.carroll@gmail.com>
2014-01-06 10:38:54 -0500

    alert, sign-in, out, setting working

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+
 	validates_presence_of :name
 	validates_uniqueness_of :name, :email, :case_sensitive => false
 
