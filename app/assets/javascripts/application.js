f20c6f7acc9d6d4a6d098ff3a0f727d0f730c0be
Corwin Carroll <corwin.p.carroll@gmail.com>
2014-01-10 14:16:32 -0500

    jq update for canvas slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,7 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+$('a.left-off-canvas-toggle').click(function() {
+  $('.inner-wrap').css('min-height', $(window).height() - $('footer').outerHeight() + 'px');
+});
