96d25ffbd4b80dcb50c293dd522235c955b6db47
Corwin Carroll <corwin.p.carroll@gmail.com>
2014-01-15 12:33:39 -0500

    debugging jquery

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,77 @@
 class User < ActiveRecord::Base
   rolify
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :stripe_token, :coupon
+  attr_accessor :stripe_token, :coupon
+ 
+ 	validates_presence_of :name
+	validates_uniqueness_of :name, :email, :case_sensitive => false 
 
-	validates_presence_of :name
-	validates_uniqueness_of :name, :email, :case_sensitive => false
+  before_save :update_stripe
+
+  def update_stripe
+		return if email.include?(ENV['ADMIN_EMAIL'])
+		return if email.include?('@example.com') and not Rails.env.production?
+		if customer_id.nil?
+			if !stripe_token.present? && roles.first.name != 'free'
+				raise "Stripe token not present. Can't create account."
+			end
+		if roles.first.name == 'free_iosbc'
+		if coupon.blank?
+			customer = Stripe::Customer.create(
+				:email => email,
+				:description => name,
+				:plan => roles.first.name
+			)
+		else
+			customer = Stripe::Customer.create(
+				:email => email,
+				:description => name,
+				:plan => roles.first.name,
+				:coupon => coupon
+			)
+		end
+	else
+		if coupon.blank?
+			customer = Stripe::Customer.create(
+				:email => email,
+				:description => name,
+				:card => stripe_token,
+				:plan => roles.first.name
+			)
+		else
+			customer = Stripe::Customer.create(
+				:email => email,
+				:description => name,
+				:card => stripe_token,
+				:plan => roles.first.name,
+				:coupon => coupon
+			)
+		end
+		end
+	else
+		customer = Stripe::Customer.retrieve(customer_id)
+		if stripe_token.present?
+			customer.card = stripe_token
+		end
+		customer.email = email
+		customer.description = name
+		customer.save
+	end
+self.last_4_digits = customer.active_card.last4 unless roles.first.name == 'free'
+self.customer_id = customer.id
+self.stripe_token = nil
+rescue Stripe::StripeError => e
+	logger.error "Stripe Error: " + e.message
+	errors.add :base, "#{e.message}."
+	self.stripe_token = nil
+	false
+end
 
 end
