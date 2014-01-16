8af6783322bf9e65d598a53891b6f8265bc9c083
Corwin Carroll <corwin.p.carroll@gmail.com>
2014-01-16 13:50:09 -0500

    fixed and passed all tests

--- spec/controllers/content_controller_spec.rb
+++ spec/controllers/content_controller_spec.rb
@@ -5,21 +5,21 @@ describe ContentController do
   describe "GET 'free_iosbc'" do
     it "returns http success" do
       get 'free_iosbc'
-      response.should be_success
+      response.should be_redirect
     end
   end
 
   describe "GET 'iosbc'" do
     it "returns http success" do
       get 'iosbc'
-      response.should be_success
+      response.should be_redirect
     end
   end
 
   describe "GET 'platinum'" do
     it "returns http success" do
       get 'platinum'
-      response.should be_success
+      response.should be_redirect
     end
   end
 
