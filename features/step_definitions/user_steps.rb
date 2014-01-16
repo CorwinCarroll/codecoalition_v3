50a52c4c8f3443ddea3506f9b375a7b95b8686fa
Corwin Carroll <corwin.p.carroll@gmail.com>
2014-01-16 13:34:42 -0500

    cucumber cleanup

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,12 +1,10 @@
-### UTILITY METHODS ###
-
 def create_visitor
   @visitor ||= { :name => "Testy McUserton", :email => "example@example.com",
-    :password => "changeme", :password_confirmation => "changeme" }
+    :password => "changeme", :password_confirmation => "changeme", :role => "silver" }
 end
 
 def find_user
-  @user ||= User.where(:email => @visitor[:email]).first
+  @user ||= User.first conditions: {:email => @visitor[:email]}
 end
 
 def create_unconfirmed_user
@@ -19,19 +17,20 @@ end
 def create_user
   create_visitor
   delete_user
-  @user = FactoryGirl.create(:user, @visitor)
+  @user = FactoryGirl.create(:user, email: @visitor[:email])
+  @user.add_role(@visitor[:role])
 end
 
 def delete_user
-  @user ||= User.where(:email => @visitor[:email]).first
+  @user ||= User.first conditions: {:email => @visitor[:email]}
   @user.destroy unless @user.nil?
 end
 
 def sign_up
   delete_user
-  visit '/users/sign_up'
-  fill_in "user_name", :with => @visitor[:name]
-  fill_in "user_email", :with => @visitor[:email]
+  visit '/users/sign_up/?plan=free_iosbc'
+  fill_in "Name", :with => @visitor[:name]
+  fill_in "Email", :with => @visitor[:email]
   fill_in "user_password", :with => @visitor[:password]
   fill_in "user_password_confirmation", :with => @visitor[:password_confirmation]
   click_button "Sign up"
@@ -40,14 +39,14 @@ end
 
 def sign_in
   visit '/users/sign_in'
-  fill_in "user_email", :with => @visitor[:email]
-  fill_in "user_password", :with => @visitor[:password]
+  fill_in "Email", :with => @visitor[:email]
+  fill_in "Password", :with => @visitor[:password]
   click_button "Sign in"
 end
 
 ### GIVEN ###
 Given /^I am not logged in$/ do
-  visit '/users/sign_out'
+  visit destroy_user_session_path
 end
 
 Given /^I am logged in$/ do
@@ -101,6 +100,10 @@ When /^I sign up without a password$/ do
   sign_up
 end
 
+When /^I sign up without a subscription plan$/ do
+  visit '/users/sign_up'
+end
+
 When /^I sign up with a mismatched password confirmation$/ do
   create_visitor
   @visitor = @visitor.merge(:password_confirmation => "changeme123")
@@ -121,15 +124,21 @@ When /^I sign in with a wrong password$/ do
   sign_in
 end
 
-When /^I edit my account details$/ do
+When /^I change my email address$/ do
   click_link "Edit account"
-  fill_in "user_name", :with => "newname"
+  fill_in "user_email", :with => "different@example.com"
   fill_in "user_current_password", :with => @visitor[:password]
   click_button "Update"
 end
 
-When /^I look at the list of users$/ do
-  visit '/'
+When /^I delete my account$/ do
+  click_link "Edit account"
+  click_link "Cancel my account"
+  page.driver.browser.switch_to.alert.accept
+end
+
+When /^I follow the subscribe for silver path$/ do
+  visit '/users/sign_up/?plan=silver'
 end
 
 ### THEN ###
@@ -140,9 +149,20 @@ Then /^I should be signed in$/ do
 end
 
 Then /^I should be signed out$/ do
-  page.should have_content "Sign up"
   page.should have_content "Login"
   page.should_not have_content "Logout"
+end
+
+Then /^I should see "(.*?)"$/ do |text|
+  page.should have_content text
+end
+
+Then /^I should be on the "([^"]*)" page$/ do |path_name|
+  current_path.should == send("#{path_name.parameterize('_')}_path")
+end
+
+Then /I should be on the new silver user registration page$/ do
+  current_path_with_args.should == '/users/sign_up/?plan=silver'
 end
 
 Then /^I see an unconfirmed account message$/ do
@@ -154,23 +174,27 @@ Then /^I see a successful sign in message$/ do
 end
 
 Then /^I should see a successful sign up message$/ do
-  page.should have_content "A message with a confirmation link has been sent to your email address."
+  page.should have_content "Welcome! You have signed up successfully."
 end
 
 Then /^I should see an invalid email message$/ do
-  page.should have_content "Email is invalid"
+  page.should have_content "is invalid"
 end
 
 Then /^I should see a missing password message$/ do
-  page.should have_content "Password can't be blank"
+  page.should have_content "can't be blank"
 end
 
 Then /^I should see a missing password confirmation message$/ do
-  page.should have_content "Password doesn't match confirmation"
+  page.should have_content "doesn't match Password"
 end
 
 Then /^I should see a mismatched password message$/ do
-  page.should have_content "Password doesn't match confirmation"
+  page.should have_content "doesn't match Password"
+end
+
+Then /^I should see a missing subscription plan message$/ do
+  page.should have_content "Please select a subscription plan below"
 end
 
 Then /^I should see a signed out message$/ do
@@ -183,6 +207,10 @@ end
 
 Then /^I should see an account edited message$/ do
   page.should have_content "You updated your account successfully."
+end
+
+Then /^I should see an account deleted message$/ do
+  page.should have_content "account was successfully cancelled"
 end
 
 Then /^I should see my name$/ do
